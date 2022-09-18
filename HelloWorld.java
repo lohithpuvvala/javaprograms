ed0e2ef40a51c9a086522134a804ca3179a5e154
lohithpuvvala <lohithpuvvala@gmail.com>
2022-09-18 15:35:17 +0530

    commit and push HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 	public static void main (String[] args) {
 		//prints "Hello, World" to the terminal window.
-		System.out.println("Hello, lohith");
+		System.out.println("Hello, World!!");
 	}
 }
